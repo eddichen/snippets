be1d384b6b7d7d705b07f1c2275f710b7d14fff0
eddichen <contacteddichen@gmail.com>
2017-07-15 11:24:27 +0100

    updating file paths

--- service-worker/sw.js
+++ service-worker/sw.js
@@ -1,17 +1,29 @@
 var CACHE_NAME = 'sw-example-cache';
 var urlsToCache = [
-    './index.html',
-    './css/normalize.css'
+    '/',
+    '/css/normalize.css'
 ];
 
 self.addEventListener('install', function(event) {
     // Perform install steps
-    console.log('service worker install');
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(function(cache) {
-                console.log('Opened cache');
                 return cache.addAll(urlsToCache);
             })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+        )
     );
 });
