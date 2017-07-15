5d214e59898a4f11cecd1444b9ee07d047e849dd
eddichen <contacteddichen@gmail.com>
2017-07-15 09:02:59 +0100

    updating the service pwrker

--- service-worker/sw.js
+++ service-worker/sw.js
@@ -1,16 +1,17 @@
 var CACHE_NAME = 'sw-example-cache';
 var urlsToCache = [
-    '/service-worker/index.html',
-    './css/normalize.css'
+    'index.html',
+    'css/normalize.css'
 ];
 
 self.addEventListener('install', function(event) {
     // Perform install steps
+    console.log('service worker install');
     event.waitUntil(
-      caches.open(CACHE_NAME)
-        .then(function(cache) {
-          console.log('Opened cache');
-          return cache.addAll(urlsToCache);
-        })
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
     );
 });
