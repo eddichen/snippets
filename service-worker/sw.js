ad6a653952ae4b8e16c61f0f04b45f6dd4d30c99
eddichen <contacteddichen@gmail.com>
2017-07-15 08:56:38 +0100

    installing service worker

--- service-worker/sw.js
+++ service-worker/sw.js
@@ -1,4 +1,16 @@
+var CACHE_NAME = 'sw-example-cache';
+var urlsToCache = [
+    '/service-worker/index.html',
+    './css/normalize.css'
+];
+
 self.addEventListener('install', function(event) {
-  // Perform install steps
-  console.log('install');
+    // Perform install steps
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(function(cache) {
+          console.log('Opened cache');
+          return cache.addAll(urlsToCache);
+        })
+    );
 });
